c1a76dba26f4815812fb537194917fde005f9972
Kristiina <kristiina.ra@gmail.com>
2022-01-06 11:55:42 +0200

    Added automatic downloading and redownloading of official cpe xml file.

--- DependencyChecker/CPEAnalysis/CPEFinder.swift
+++ DependencyChecker/CPEAnalysis/CPEFinder.swift
@@ -14,6 +14,7 @@ class CPEFinder {
     var folder: URL
     var changed = false
     let settings: Settings
+    var cpePath: URL
     
     init(settings: Settings) {
         self.folder = settings.homeFolder
@@ -31,6 +32,82 @@ class CPEFinder {
         }
         
         self.settings = settings
+        self.cpePath = self.settings.homeFolder.appendingPathComponent("official-cpe-dictionary_v2.3.xml", isDirectory: false)
+        
+        if self.checkCPEDatafile() == false {
+            self.downloadCPEDataFile()
+        }
+        
+        if self.shouldUpdate {
+            self.update()
+        }
+    }
+    
+    var shouldUpdate: Bool {
+        if let timeInterval = self.settings.cpeTimeInterval {
+            // check if time since last updated is larger than the allowed timeinterval for updates
+            if self.cpeDictionary.lastUpdated.timeIntervalSinceNow * -1 > timeInterval {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func update() {
+        os_log("update cpe data file")
+        self.updateCPEDataFile()
+        var updatedCPEs: [String: CPE] = [:]
+        
+        for cpe in self.cpeDictionary.dictionary {
+            if cpe.value.value == nil {
+                // ignore, these will be removed from cpes so that they can be updated
+            } else {
+                updatedCPEs[cpe.key] = cpe.value
+            }
+        }
+        self.cpeDictionary.lastUpdated = Date()
+        self.cpeDictionary.dictionary = updatedCPEs
+        
+        self.changed = true
+    }
+    
+    func checkCPEDatafile() -> Bool{
+        let path = self.cpePath.path
+        
+        os_log("check cpe data path: \(path)")
+        let pathExists = FileManager.default.fileExists(atPath: path)
+        os_log("path exists: \(pathExists)")
+        
+        return pathExists
+    }
+    
+    func downloadCPEDataFile() {
+        let downloadPath = "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.gz"
+        if let downloadURL = URL(string: downloadPath) {
+            let gzipPath = self.cpePath.appendingPathExtension("gz")
+            
+            if let data = try? Data(contentsOf: downloadURL) {
+                do {
+                    try data.write(to: gzipPath)
+                    let res = Helper.shell(launchPath: "/usr/bin/gzip", arguments: ["-d", gzipPath.path])
+                    os_log("Unzipping: \(res)")
+                } catch {
+                    os_log("Downloading official cpe dictionary failed: \(error.localizedDescription)")
+                }
+            }
+        } else {
+            os_log("Download path not a valid URL \(downloadPath)")
+        }
+    }
+    
+    func updateCPEDataFile() {
+        do {
+            try FileManager.default.removeItem(at: self.cpePath)
+        } catch {
+            os_log("Removing cpe dictionary failed: \(error.localizedDescription)")
+        }
+        self.downloadCPEDataFile()
     }
     
     deinit {
@@ -69,10 +146,11 @@ class CPEFinder {
         }
         
         if name.contains("/") {
-            let cpePath = "/Users/kristiina/Phd/Tools/GraphifyEvolution/ExternalAnalysers/CPE/official-cpe-dictionary_v2.3.xml"
+            let cpePath = self.cpePath.path
 
             if FileManager.default.fileExists(atPath: cpePath) {
                 os_log("cpe for title: \(name)")
+                os_log("querying from file: \(cpePath)")
                 
                 let output = Helper.shell(launchPath: "/bin/zsh", arguments: ["-c", "grep -A3 -i -e \(name) \(cpePath) | grep \"<cpe-23:cpe23-item name\""])
                 os_log("cpe output: \(output)")
