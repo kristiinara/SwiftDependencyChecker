702e5624facb66f09e9b16985fb7d3f1b5865516
Kristiina <kristiina.ra@gmail.com>
2022-01-10 13:29:06 +0200

    Renamed folders to match project name

--- Package.swift
+++ Package.swift
@@ -17,12 +17,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
-            name: "DependencyChecker",
+            name: "SwiftDependencyChecker",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Gzip", package: "GzipSwift")]),
         .testTarget(
-            name: "DependencyCheckerTests",
-            dependencies: ["DependencyChecker"]),
+            name: "SwiftDependencyCheckerTests",
+            dependencies: ["SwiftDependencyChecker"]),
     ]
 )
